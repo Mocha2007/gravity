57ef9bf6ed3766c6146f965d349fcb9f627b7488
mocha <mocha2007andcoco@gmail.com>
2018-03-02 14:23:03 -0500

    quick tool for making 2-bit mokifiles

---
+++ mokitools.py
@@ -0,0 +1,7 @@
+def twobit(colorstring):
+	'''w = white, s = silver, g = grey, b = black'''
+	return int('0b'+colorstring.replace('w','11').replace('s','10').replace('g','01').replace('b','00'),2)
+
+while 1:
+	try:print(twobit(input('s?\n> ')))
+	except:pass
