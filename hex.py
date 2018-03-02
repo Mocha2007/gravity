963125066146bc37a8039c1b5bafaa360b10b306
mocha <mocha2007andcoco@gmail.com>
2018-03-02 10:47:39 -0500

    added simple hex editor

---
+++ hex.py
@@ -0,0 +1,12 @@
+filename = input('Filename\n> ')
+ha = ''
+while 1:
+	try:print(ha)
+	except:pass
+	current = input('Hex\n> ').lower()
+	if current=='end':break
+	if current=='moki':ha+=''.join(list(map(chr,[77,79,75,73,3,0,5,0,5,1,240,0,0,1,0,16])))
+	else:
+		try:ha+=chr(int('0'+(current[0] if current[0] in 'box' else '')+current[1:],{'b':2,'o':8,'x':16}[current[0]]))
+		except:print('Invalid Number')
+open(filename, "w").write(ha)
