9b8e570a8ccb15b159ced059b098da894f639153
mocha <mocha2007andcoco@gmail.com>
2018-03-01 21:03:23 -0500

    added image viewers for... no reason whatsoever

---
+++ mokiview.py
@@ -0,0 +1,69 @@
+import binascii,pygame
+from sys import exit
+pygame.init()
+
+loc = input('Location\n> ')# mokiview.py or test.moki
+
+# https://stackoverflow.com/a/34687617/2579798
+with open(loc, 'rb') as f:image=binascii.hexlify(f.read())
+
+# create the image array
+image = str(image)[2:-1]
+ia = []
+for i in range(0,len(image),2):#convert to a decimal array OwO
+	ia+=[int('0x'+image[i:i+2],16)]
+
+def findsize(bmp):#w,h
+	w = bmp[6]+256*bmp[5]
+	h = bmp[8]+256*bmp[7]
+	return w,h
+
+def findstart(bmp):
+	return bmp[15]+256*bmp[14]
+
+mul = 512/max(findsize(ia))
+if mul<.2:exit()#safety net
+size = int(findsize(ia)[0]*mul),int(findsize(ia)[1]*mul)
+screen = pygame.display.set_mode(size)
+
+def zeromatrix(x,y):
+	a = []
+	for i in range(x):
+		a+=[[0]*y]
+	return a
+
+def getpixeldata(bmp):#w,h
+	start = findstart(bmp)
+	imgsize = findsize(bmp)
+	data = zeromatrix(imgsize[0],imgsize[1])
+	bs = bmp[start:]
+	for i in range(0,len(bs),3):
+		try:
+			tribyte = (bs[i],bs[i+1],bs[i+2])
+			coord = round(i/3%imgsize[1]),round(i/3//imgsize[1])
+			data[coord[0]][coord[1]] = tribyte
+		except:print('ERROR loading',coord)
+	return data
+
+print('Loading image...')
+final = getpixeldata(ia)#[::-1]
+fsize = findsize(ia)
+
+#disp
+
+print('Rendering image...')
+for col in range(fsize[0]):
+	for row in range(fsize[1]):
+		pixel = final[col][row]
+		pygame.draw.rect(screen,pixel,pygame.Rect(col*mul,row*mul,mul,mul))
+		if pixel==0:print('ERROR rendering',(col,row))
+pygame.display.flip()
+
+#events
+while 1:
+	ev = pygame.event.get()
+	for event in ev:
+		if event.type == pygame.MOUSEBUTTONUP:pass
+		elif event.type == pygame.QUIT:
+			pygame.quit()
+			exit()
